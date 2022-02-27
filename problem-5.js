0549972386a8e58509d030cb210d1f8b8b43efa8
zishan344 <zishanahmed344@gmail.com>
2022-02-27 10:29:48 +0600

    done the problem  numbers 5

---
+++ problem-5.js
@@ -0,0 +1,9 @@
+/* ৫. জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো।  */
+
+//  number 1  map  ar kaj ki : ans: map array teke prottek upadan loop kore function ke call kore sob function ar result akta noton array te push kore . amader ar akta noton array dei
+
+// number2 forEach ar kaj ki . ans: forEach akti array prti ti upadaner jonno akti function call kore kinto kiso return korena
+
+// number3 filter: filter array ar proti ti upadaner modde jeta jeta sorto full fill korbe taderke new akta array te reke setake return kore .
+
+//number 4 find: ans : arrry ar protom je upadan sorto full fill korbe sodo sei upadanke return korbe . baki golo full fill korle o return korbe na . protome jeta sorto full fill kore sodo matro seta retunrn kore akta noton array hisabe
